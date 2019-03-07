dcebc830b49307d90e0ac723f1c720a8e7029b01
Bartlomiej Lisiecki <bartlomiej.lisiecki@rumblefishdev.com>
2019-03-07 17:03:25 +0100

    Store WIP

--- src/renderer/store/reducers.js
+++ src/renderer/store/reducers.js
@@ -1,11 +1,13 @@
 import { combineReducers } from 'redux-immutable'
 
 import nodeHandlers from './handlers/node'
+import ratesHandlers from './handlers/rates'
 import vaultHandlers from './handlers/vault'
 import vaultCreatorHandlers from './handlers/vaultCreator'
 import vaultUnlockerHandlers from './handlers/vaultUnlocker'
 
 export default combineReducers({
+  rates: ratesHandlers.reducer,
   node: nodeHandlers.reducer,
   vault: vaultHandlers.reducer,
   vaultCreator: vaultCreatorHandlers.reducer,
