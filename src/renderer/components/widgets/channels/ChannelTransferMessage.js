0dba29bf658187ad1d8fbdc6094002d23a3d2dca
Norbert Bodziony <norbert.bodziony@rumblefish.pl>
2019-08-07 10:01:03 +0200

    create basic message and update message/transfer

--- src/renderer/components/widgets/channels/ChannelTransferMessage.js
+++ src/renderer/components/widgets/channels/ChannelTransferMessage.js
@@ -1,72 +1,29 @@
 import React, { useState } from 'react'
 import PropTypes from 'prop-types'
-import { DateTime } from 'luxon'
-import classNames from 'classnames'
+import BigNumber from 'bignumber.js'
 import * as R from 'ramda'
 
-import Grid from '@material-ui/core/Grid'
 import Link from '@material-ui/core/Link'
+import Grid from '@material-ui/core/Grid'
 import Divider from '@material-ui/core/Divider'
 import Typography from '@material-ui/core/Typography'
-import ListItem from '@material-ui/core/ListItem'
 import Collapse from '@material-ui/core/Collapse'
-import ListItemText from '@material-ui/core/ListItemText'
-import CircularProgress from '@material-ui/core/CircularProgress'
 import { withStyles } from '@material-ui/core/styles'
 
 import red from '@material-ui/core/colors/red'
-import lightGreen from '@material-ui/core/colors/lightGreen'
 
-import DoneIcon from '@material-ui/icons/Done'
-import DoneAllIcon from '@material-ui/icons/DoneAll'
-import ErrorIcon from '@material-ui/icons/ErrorOutline'
-import BlockIcon from '@material-ui/icons/Block'
-
-import { getZbayAddress } from '../../../zbay/channels'
 import { _DisplayableMessage } from '../../../zbay/messages'
-import Elipsis from '../../ui/Elipsis'
 import ChannelMessageActions from './ChannelMessageActions'
 
 import ZecBalance from '../walletPanel/ZecBalance'
 import SpinnerLoaderComponent from '../../ui/SpinnerLoader'
-import BigNumber from 'bignumber.js'
+import Elipsis from '../../ui/Elipsis'
+import BasicMessage from './BasicMessage'
 
 const styles = theme => ({
-  messageCard: {
-    padding: 0
-  },
   transactionCard: {
-    paddingTop: theme.spacing.unit,
-    paddingBottom: theme.spacing.unit
-  },
-  wrapper: {
-    background: '#fff',
-    marginBottom: theme.spacing.unit
-  },
-  clickable: {
-    cursor: 'pointer'
-  },
-  wrapperPending: {
-    background: '#eeeeee'
-  },
-  username: {
-    fontSize: '0.855rem'
-  },
-  message: {
-    fontSize: '0.855rem',
-    marginTop: theme.spacing.unit,
-    whiteSpace: 'pre-line'
-  },
-  statusIcon: {
-    color: theme.typography.caption.color,
-    fontSize: '0.95rem',
-    marginLeft: theme.spacing.unit
-  },
-  broadcasted: {
-    color: lightGreen[600]
-  },
-  failed: {
-    color: red[500]
+    paddingTop: 2 * theme.spacing.unit,
+    paddingBottom: 2 * theme.spacing.unit
   },
   divider: {
     minWidth: `calc(100% + ${4 * theme.spacing.unit}px)`,
@@ -75,32 +32,24 @@ const styles = theme => ({
     height: 2
   },
   link: {
-    color: theme.palette.colors.blue
+    color: theme.palette.colors.blue,
+    fontSize: '0.9rem'
   },
   info: {
     paddingTop: theme.spacing.unit
+  },
+  failed: {
+    color: red[500]
+  },
+  boldText: {
+    fontWeight: '500'
+  },
+  message: {
+    fontSize: '0.855rem',
+    marginTop: theme.spacing.unit,
+    whiteSpace: 'pre-line'
   }
 })
-
-const statusComponent = {
-  broadcasted: DoneAllIcon,
-  pending: props => <CircularProgress size={12} {...props} />,
-  success: DoneIcon,
-  failed: ErrorIcon,
-  cancelled: BlockIcon
-}
-
-const getTimeFormat = time => {
-  const today = DateTime.utc()
-  if (time.hasSame(today, 'day')) {
-    return 'T'
-  } else if (time.hasSame(today, 'week')) {
-    return 'ccc, HH:mm'
-  } else if (time.hasSame(today, 'year')) {
-    return 'LLL dd, HH:mm'
-  }
-  return 'LLL dd, y, HH:mm'
-}
 
 export const ChannelTransferMessage = ({
   classes,
@@ -110,122 +59,81 @@ export const ChannelTransferMessage = ({
   onReply,
   onCancel
 }) => {
+  const [actionsOpen, setActionsOpen] = useState(null)
+
   const tnx = message.get('id')
   const fromYou = message.get('fromYou', false)
-  const [actionsOpen, setActionsOpen] = useState(null)
-  const sender = message.get('sender')
-  const username = sender.get('username', 'Unnamed')
-  const address = getZbayAddress(sender.get('replyTo'))
   const spentZec = message.get('spent')
   const spentUsd = rateUsd.times(new BigNumber(message.get('spent') || 0)).toFormat(2)
   const info = message.get('message')
-
-  const time = DateTime.fromSeconds(message.get('createdAt'))
-  const timeFormat = getTimeFormat(time)
-  const timeString = time.toFormat(timeFormat)
+  const receiver = message.get('receiver')
+  const receiverUsername = receiver.get('username', 'Unnamed')
 
   const status = message.get('status', 'broadcasted')
-  const StatusIcon = statusComponent[status]
   const error = message.get('error')
-
   return (
-    <ListItem
-      className={classNames({
-        [classes.wrapper]: true,
-        [classes.clickable]: ['failed', 'cancelled'].includes(status),
-        [classes.wrapperPending]: status !== 'broadcasted'
-      })}
-      alignItems='flex-start'
-      onClick={() => setActionsOpen(!actionsOpen)}
-    >
-      <ListItemText
-        disableTypography
-        className={classes.messageCard}
-        primary={
-          <Grid container direction='row' justify='space-between' alignItems='flex-start'>
-            <Grid item>
-              <Typography color='textPrimary' className={classes.username}>
-                {username}
-                {fromYou ? ' (You)' : null}
+    <BasicMessage message={message} actionsOpen={actionsOpen} setActionsOpen={setActionsOpen}>
+      <React.Fragment>
+        <Divider className={classes.divider} />
+        <Grid container direction='column' justify='space-evenly' alignItems='center'>
+          <Grid
+            container
+            direction='column'
+            justify='space-evenly'
+            alignItems='center'
+            className={classes.transactionCard}
+            spacing={0}
+          >
+            <Grid item xs={12}>
+              <Typography variant='h4' className={classes.boldText}>
+                ${spentUsd}
               </Typography>
-              <Typography variant='caption'>{address.substring(0, 32)}...</Typography>
             </Grid>
-            <Grid item>
-              <Grid container direction='row' alignItems='center' justify='flex-end'>
-                <Typography variant='caption'>{timeString}</Typography>
-                <StatusIcon
-                  className={classNames({
-                    [classes.statusIcon]: true,
-                    [classes.failed]: status === 'failed',
-                    [classes.broadcasted]: status === 'broadcasted'
-                  })}
-                />
-              </Grid>
-              {status === 'failed' ? (
-                <Elipsis
-                  interactive
-                  content={`Error ${error.code}: ${error.message}`}
-                  tooltipPlacement='bottom'
-                  length={60}
-                  classes={{ content: classes.failed }}
-                />
-              ) : null}
+            <Grid item xs={12}>
+              <Typography variant='h4' className={classes.boldText}>
+                <ZecBalance size={20} value={spentZec} />
+              </Typography>
+            </Grid>
+            <Grid item xs={12}>
+              <Typography variant='subtitle1' className={classes.boldText}>
+                Sent to :
+              </Typography>
+            </Grid>
+            <Grid item xs={12}>
+              <Typography variant='subtitle1' className={classes.boldText}>
+                {fromYou ? 'You' : receiverUsername}
+              </Typography>
+            </Grid>
+            <Grid item xs={12}>
+              {showStatusInfo({ status, classes, receiver, tnx, error })}
             </Grid>
           </Grid>
-        }
-        secondary={
-          <React.Fragment>
-            <Divider className={classes.divider} />
-            <Grid container direction='column' justify='space-evenly' alignItems='center'>
-              <Grid
-                container
-                direction='column'
-                justify='space-evenly'
-                alignItems='center'
-                className={classes.transactionCard}
-              >
-                <Grid item xs={12}>
-                  <Typography variant='display2'>${spentUsd}</Typography>
-                </Grid>
-                <Grid item xs={12}>
-                  <ZecBalance value={spentZec} />
-                </Grid>
-                <Grid item xs={12}>
-                  <Typography variant='title'>Send to :</Typography>
-                </Grid>
-                <Grid item xs={12}>
-                  <Typography variant='title'>{fromYou ? 'You' : username}</Typography>
-                </Grid>
-                <Grid item xs={12}>
-                  {showStatusInfo({ status, classes, tnx, error })}
-                </Grid>
-              </Grid>
-              <Divider className={classes.divider} />
-              <Grid
-                container
-                direction='row'
-                alignItems='center'
-                justify='flex-start'
-                className={classes.info}
-              >
-                <Grid item xs={12}>
-                  <Typography variant='Body 2'>{info}</Typography>{' '}
-                </Grid>
-              </Grid>
-              <Collapse in timeout='auto'>
-                <ChannelMessageActions
-                  onReply={() => onReply(message)}
-                  onResend={() => onResend(message)}
-                  onCancel={onCancel}
-                  fromYou={fromYou}
-                  status={status}
-                />
-              </Collapse>
+          <Divider className={classes.divider} />
+          <Grid
+            container
+            direction='row'
+            alignItems='center'
+            justify='flex-start'
+            className={classes.info}
+          >
+            <Grid item xs={12}>
+              <Typography variant='body2' className={classes.message}>
+                {info}
+              </Typography>
             </Grid>
-          </React.Fragment>
-        }
-      />
-    </ListItem>
+          </Grid>
+          <Collapse in={actionsOpen} timeout='auto'>
+            <ChannelMessageActions
+              onReply={() => onReply(message)}
+              onResend={() => onResend(message)}
+              onCancel={onCancel}
+              fromYou={fromYou}
+              status={status}
+            />
+          </Collapse>
+        </Grid>
+      </React.Fragment>
+    </BasicMessage>
   )
 }
 
@@ -234,9 +142,7 @@ const showStatusInfo = ({ status, classes, tnx, error }) => {
     case 'broadcasted':
       return (
         <Link
-          onClick={
-            () => {} // send to address
-          }
+          onClick={() => window.open(`https://explorer.zcha.in/transactions/${tnx}`)}
           variant='body1'
           className={classes.link}
         >
