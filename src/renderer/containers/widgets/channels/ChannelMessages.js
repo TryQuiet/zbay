f4c3a7f88aec7fc0db8dbd1867d8c0aa57e7b164
Janusz Kornaga <janusz.kornaga@rumblefishdev.com>
2019-08-14 11:38:03 +0200

    save wip

--- src/renderer/containers/widgets/channels/ChannelMessages.js
+++ src/renderer/containers/widgets/channels/ChannelMessages.js
@@ -5,6 +5,7 @@ import { bindActionCreators } from 'redux'
 import ChannelMessagesComponent from '../../../components/widgets/channels/ChannelMessages'
 import channelSelectors from '../../../store/selectors/channel'
 import messagesHandlers from '../../../store/handlers/messages'
+import directMessagesHandlers from '../../../store/handlers/contacts'
 import { useInterval } from '../../hooks'
 
 export const mapStateToProps = state => ({
@@ -12,11 +13,13 @@ export const mapStateToProps = state => ({
 })
 
 export const mapDispatchToProps = dispatch => bindActionCreators({
-  fetchMessages: messagesHandlers.epics.fetchMessages
+  fetchMessages: messagesHandlers.epics.fetchMessages,
+  directMessagesHandlers: directMessagesHandlers.epics.fetchMessages
 }, dispatch)
 
-export const ChannelMessages = ({ className, messages, loadMessages, fetchMessages, loader }) => {
+export const ChannelMessages = ({ className, messages, loadMessages, directMessagesHandlers, fetchMessages, loader }) => {
   useInterval(fetchMessages, 15000)
+  useInterval(directMessagesHandlers, 2000)
   return (
     <ChannelMessagesComponent messages={messages} loader={loader} />
   )
