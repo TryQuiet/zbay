0b6e1563610f7a53fee913501936a02baa66cc9f
Norbert Bodziony <norbert.bodziony@rumblefish.pl>
2020-07-10 09:43:51 +0200

    fix shipping form initialvalues

--- src/renderer/store/handlers/identity.js
+++ src/renderer/store/handlers/identity.js
@@ -424,7 +424,7 @@ export const setIdentityEpic = (identityToSet, isNewUser) => async (
       )
       identity = updatedIdentity
     }
-    await dispatch(setIdentity(identity))
+    await dispatch(setIdentity(Identity(identity)))
     await dispatch(txnTimestampsHandlers.epics.getTnxTimestamps())
     dispatch(removedChannelsHandlers.epics.getRemovedChannelsTimestamp())
 
