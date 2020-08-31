b0180e45d4a7338e15c7bfd8091335ce5e65d336
Norbert <norbertbodziony@gmail.com>
2020-08-31 09:46:26 +0200

    open vault before migration

--- src/renderer/store/handlers/update.js
+++ src/renderer/store/handlers/update.js
@@ -1,10 +1,27 @@
 import { ipcRenderer } from 'electron'
 import { actionCreators } from './modals'
 import { createMigrationFile } from './identity'
+import vaultHandlers from './vault'
+import electronStore from '../../../shared/electronStore'
+import nodeSelectors from '../selectors/node'
+
 export const checkForUpdate = () => async (dispatch, getState) => {
   // create file on new update
   // Note it will recreate file on each new update so file will be up to date
-  await dispatch(createMigrationFile())
+
+  const vaultPassword = electronStore.get('vaultPassword')
+  if (vaultPassword) {
+    const network = nodeSelectors.network(getState())
+    await dispatch(
+      vaultHandlers.actions.unlockVault({
+        masterPassword: vaultPassword,
+        network,
+        ignoreError: true
+      })
+    )
+    await dispatch(createMigrationFile())
+  }
+
   dispatch(actionCreators.openModal('applicationUpdate')())
 }
 
